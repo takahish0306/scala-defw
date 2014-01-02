006ec26c8308c53b24c498795ea0c36969069308
Takahiro Ishikawa <takahish0306@yahoo.co.jp>
2014-01-02 17:17:16 +0900

    Added config Config

--- src/test/scala/com/github/takahish0306/framework/config/EvalTest.scala
+++ src/test/scala/com/github/takahish0306/framework/config/EvalTest.scala
@@ -15,10 +15,3 @@ class EvalTest extends FlatSpec with Eval {
   }
 
 }
-
-trait Test {
-  val value1: String
-  val value2: Int
-  case class Value3 (value4: String, value5: String)
-  val value3: Value3
-}
